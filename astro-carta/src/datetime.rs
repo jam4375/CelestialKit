7915510634a08e1ab67a85fc8fd5c0351c0450d6
Jason Monschke <jam4375@gmail.com>
2024-03-16 16:32:56 -0500

    Partial implementation of the DateTime struct and added a new utils module

--- astro-carta/src/datetime.rs
+++ astro-carta/src/datetime.rs
@@ -1,8 +1,47 @@
 mod month;
 pub mod timedelta;
+mod utils;
 
 pub use timedelta::TimeDelta;
 
+/// Represents an instant in time
 pub struct DateTime {
+    /// Duration since the implicit epoch of 0001-01-01 00:00:00 TAI
     duration: TimeDelta,
+}
+
+impl DateTime {
+    pub fn new(
+        year: i32,
+        month: i32,
+        day: i32,
+        hour: i32,
+        minute: i32,
+        second: f64,
+    ) -> Option<Self> {
+        if !utils::is_valid_year_month_day(year, month, day)
+            || hour < 0
+            || hour > 23
+            || minute < 0
+            || minute > 59
+            || second < 0.0
+            || second >= 60.0
+        {
+            return None;
+        }
+
+        let prev_year = year - 1;
+        let doy = utils::day_of_year(year, month, day)?;
+        let abs_days =
+            doy - 1 + 365 * prev_year + prev_year / 4 - prev_year / 100 + prev_year / 400;
+
+        Some(DateTime {
+            duration: TimeDelta::new(
+                abs_days as i128 * timedelta::NANOSECONDS_PER_DAY
+                    + hour as i128 * timedelta::NANOSECONDS_PER_HOUR
+                    + minute as i128 * timedelta::NANOSECONDS_PER_MINUTE
+                    + (second * timedelta::NANOSECONDS_PER_SECOND as f64) as i128,
+            ),
+        })
+    }
 }
---
+++ astro-carta/src/datetime/utils.rs
@@ -0,0 +1,211 @@
+/// Determines whether a given year is a leap year.
+///
+/// # Arguments
+///
+/// * `year` - The year to be checked for leap year status.
+///
+/// # Returns
+///
+/// Returns `true` if the given year is a leap year, otherwise `false`.
+///
+/// # Examples
+///
+/// ```ignore
+/// assert_eq!(is_leap_year(2020), true);
+/// assert_eq!(is_leap_year(1900), false);
+/// assert_eq!(is_leap_year(2000), true);
+/// assert_eq!(is_leap_year(2024), true);
+/// assert_eq!(is_leap_year(2021), false);
+/// ```
+pub fn is_leap_year(year: i32) -> bool {
+    (((year % 4) == 0) && (year % 100) != 0) || ((year % 400) == 0)
+}
+
+/// Determines if a given year is a valid Gregorian year.
+///
+/// A valid Gregorian year should be greater than 0 and less than or equal to 10,000.
+///
+/// # Arguments
+///
+/// * `year` - An integer representing the year to be checked for validity.
+///
+/// # Returns
+///
+/// Returns `true` if the year is valid, `false` otherwise.
+///
+/// # Examples
+///
+/// ```ignore
+/// assert_eq!(is_valid_year(2024), true);
+/// assert_eq!(is_valid_year(0), false);
+/// assert_eq!(is_valid_year(10001), false);
+/// ```
+pub fn is_valid_year(year: i32) -> bool {
+    if year < 1 || year > 10_000 {
+        return false;
+    }
+
+    true
+}
+
+/// Checks if the given year and month values form a valid year-month combination.
+///
+/// # Arguments
+///
+/// * `year` - An integer representing the year.
+/// * `month` - An integer representing the month (1-12).
+///
+/// # Returns
+///
+/// `true` if the year and month values are valid, `false` otherwise.
+///
+/// # Examples
+///
+/// ```ignore
+/// assert!(is_valid_year_month(2024, 3));
+/// assert!(!is_valid_year_month(2024, 13));
+/// assert!(!is_valid_year_month(0, 3));
+/// ```
+pub fn is_valid_year_month(year: i32, month: i32) -> bool {
+    if !is_valid_year(year) {
+        return false;
+    }
+
+    if month < 1 || month > 12 {
+        return false;
+    }
+
+    true
+}
+
+/// Checks if the provided year, month, and day form a valid date.
+///
+/// # Arguments
+///
+/// * `year` - An integer representing the year.
+/// * `month` - An integer representing the month (1-based, January is 1).
+/// * `day` - An integer representing the day of the month.
+///
+/// # Returns
+///
+/// A boolean value indicating whether the provided year, month, and day form a valid date (`true`)
+/// or not (`false`).
+///
+/// # Examples
+///
+/// ```ignore
+/// assert_eq!(is_valid_year_month_day(2024, 3, 16), true);
+/// assert_eq!(is_valid_year_month_day(2024, 2, 29), true); // Leap year
+/// assert_eq!(is_valid_year_month_day(2024, 2, 30), false); // Not a valid day in February
+/// assert_eq!(is_valid_year_month_day(2024, 4, 31), false); // Not a valid day in April
+/// assert_eq!(is_valid_year_month_day(2024, 13, 1), false); // Invalid month
+/// ```
+pub fn is_valid_year_month_day(year: i32, month: i32, day: i32) -> bool {
+    if !is_valid_year_month(year, month) {
+        return false;
+    }
+
+    let is_leap_year = is_leap_year(year);
+    if day < 1 {
+        return false;
+    }
+
+    if let Some(days_in_month) = super::month::days_in_month(month as u8, is_leap_year) {
+        if day > days_in_month as i32 {
+            return false;
+        }
+    } else {
+        return false;
+    }
+
+    return true;
+}
+
+/// Calculates the day of the year from the given year, month, and day.
+/// Returns `Some(day_of_year)` if the input is valid, otherwise returns `None`.
+///
+/// # Arguments
+///
+/// * `year` - The year (e.g., 2024).
+/// * `month` - The month as an integer (1 for January, 2 for February, etc.).
+/// * `day` - The day of the month.
+///
+/// # Returns
+///
+/// * `Some(day_of_year)` - The day of the year if the input is valid.
+/// * `None` - If the input is not a valid date.
+///
+/// # Examples
+///
+/// ```ignore
+/// assert_eq!(day_of_year(2024, 3, 16), Some(76));
+/// assert_eq!(day_of_year(2024, 2, 29), Some(60));
+/// assert_eq!(day_of_year(2024, 13, 1), None);
+/// ```
+pub fn day_of_year(year: i32, month: i32, day: i32) -> Option<i32> {
+    if !is_valid_year_month_day(year, month, day) {
+        return None;
+    }
+
+    let is_leap_year = is_leap_year(year);
+    if let Some(cumul_days) = super::month::cummulative_days_for_month(month as u8, is_leap_year) {
+        Some(day + cumul_days as i32)
+    } else {
+        None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn leap_year_test() {
+        assert_eq!(is_leap_year(2020), true);
+        assert_eq!(is_leap_year(1900), false);
+        assert_eq!(is_leap_year(2000), true);
+        assert_eq!(is_leap_year(2024), true);
+        assert_eq!(is_leap_year(2021), false);
+    }
+
+    #[test]
+    fn is_valid_year_test() {
+        assert_eq!(is_valid_year(2024), true);
+        assert_eq!(is_valid_year(0), false);
+    }
+
+    #[test]
+    fn is_valid_year_month_test() {
+        assert_eq!(is_valid_year_month(2024, 1), true);
+        assert_eq!(is_valid_year_month(2024, 12), true);
+        assert_eq!(is_valid_year_month(2024, 0), false);
+        assert_eq!(is_valid_year_month(2024, 13), false);
+        assert_eq!(is_valid_year_month(0, 12), false);
+    }
+
+    #[test]
+    fn is_valid_year_month_day_test() {
+        assert_eq!(is_valid_year_month_day(2024, 3, 1), true);
+        assert_eq!(is_valid_year_month_day(2024, 2, 29), true);
+        assert_eq!(is_valid_year_month_day(2023, 2, 29), false);
+        assert_eq!(is_valid_year_month_day(2023, 2, 28), true);
+        assert_eq!(is_valid_year_month_day(2024, 3, 31), true);
+        assert_eq!(is_valid_year_month_day(2024, 3, 32), false);
+        assert_eq!(is_valid_year_month_day(2024, 3, 0), false);
+        assert_eq!(is_valid_year_month_day(2024, 13, 1), false);
+        assert_eq!(is_valid_year_month_day(2024, -1, 1), false);
+        assert_eq!(is_valid_year_month_day(0, 3, 1), false);
+    }
+
+    #[test]
+    fn day_of_year_test() {
+        assert_eq!(day_of_year(2024, 3, 16), Some(76));
+        assert_eq!(day_of_year(2023, 2, 29), None);
+        assert_eq!(day_of_year(2024, 2, 29), Some(60));
+        assert_eq!(day_of_year(2023, 12, 31), Some(365));
+        assert_eq!(day_of_year(2024, 12, 31), Some(366));
+        assert_eq!(day_of_year(2024, 13, 1), None);
+        assert_eq!(day_of_year(2024, 1, 32), None);
+        assert_eq!(day_of_year(-1, 3, 13), None);
+    }
+}
